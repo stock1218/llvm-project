021d066c025e9d1bea78dc47c84c35225fa8ed25
John Samuels <johnsamuels1218@gmail.com>
2025-03-10 19:04:09 -0400

    get matcher to bind to each specific component of an expression to be rewritten

--- clang-tools-extra/clang-tidy/modernize/UsecheckedarithmeticCheck.cpp
+++ clang-tools-extra/clang-tidy/modernize/UsecheckedarithmeticCheck.cpp
@@ -8,16 +8,54 @@ using namespace clang::ast_matchers;
 namespace clang::tidy::modernize {
 
 void UseCheckedArithmeticCheck::registerMatchers(MatchFinder *Finder) {
-  Finder->addMatcher(traverse(TK_AsIs, binaryOperator().bind("test")), this);
+	
+  Finder->addMatcher(traverse(TK_AsIs,
+	  binaryOperator(
+		  hasOperatorName("="),
+		  hasLHS(
+			  declRefExpr().bind("dest")
+		  ),
+		  hasRHS(
+			  binaryOperator(
+				  hasOperatorName("+"),
+				  hasLHS(ignoringImpCasts(declRefExpr().bind("opOne"))),
+				  hasRHS(ignoringImpCasts(declRefExpr().bind("opTwo")))
+			  )
+		  )
+	  )),
+	  this);
 
 }
 
 void UseCheckedArithmeticCheck::check(const MatchFinder::MatchResult &Result) {
   // TODO this will be used to do the rewrite
-  const auto *test = Result.Nodes.getNodeAs<Expr>("test");
-  SourceLocation BeginLoc = test->getBeginLoc();
-  SourceLocation EndLoc = test->getEndLoc();
-  auto Diag = diag(BeginLoc, "matched here");
+  const auto *dest = Result.Nodes.getNodeAs<Expr>("dest");
+  if(dest) {
+	  SourceLocation destBeginLoc = dest->getBeginLoc();
+	  SourceLocation destEndLoc = dest->getEndLoc();
+	  auto Diag = diag(destBeginLoc, "dest");
+  } else {
+	  llvm::outs() << "Error: dest";
+  }
+
+  const auto *opOne = Result.Nodes.getNodeAs<Expr>("opOne");
+  if(opOne) {
+	  SourceLocation opOneBeginLoc = opOne->getBeginLoc();
+	  SourceLocation opOneEndLoc = opOne->getEndLoc();
+	  auto opOneDiag = diag(opOneBeginLoc, "op one");
+  } else {
+	  llvm::outs() << "Error: op one";
+  }
+
+  const auto *opTwo = Result.Nodes.getNodeAs<Expr>("opTwo");
+  if(opTwo) {
+  SourceLocation opTwoBeginLoc = opTwo->getBeginLoc();
+  SourceLocation opTwoEndLoc = opTwo->getEndLoc();
+  auto opTwoDiag = diag(opTwoBeginLoc, "op two");
+  } else {
+	  llvm::outs() << "Error: op one";
+  }
+
   return;
 }
 } // namespace clang::tidy::modernize
