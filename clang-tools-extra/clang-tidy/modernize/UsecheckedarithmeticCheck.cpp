637630886bb4f452a8a671c4df5feb42106fc926
John Samuels <johnsamuels1218@gmail.com>
2025-03-16 19:56:58 -0400

    Add fix for generic add statements

--- clang-tools-extra/clang-tidy/modernize/UsecheckedarithmeticCheck.cpp
+++ clang-tools-extra/clang-tidy/modernize/UsecheckedarithmeticCheck.cpp
@@ -58,7 +58,7 @@ void UseCheckedArithmeticCheck::fixNonDeclOperation(const MatchFinder::MatchResu
 		dest->getNameInfo().getAsString() + ", " +
 		opOne->getNameInfo().getAsString() + ", " +
 		opTwo->getNameInfo().getAsString() + ")) {\n" +
-		"assert(false);\n" + "}\n";
+		"assert(0);\n" + "}\n";
 
 	DiagnosticBuilder Diag = diag(dest->getBeginLoc(), "use checked arithmetic");
 	Diag << FixItHint::CreateInsertion(dest->getLocation(), replacement);
@@ -67,21 +67,24 @@ void UseCheckedArithmeticCheck::fixNonDeclOperation(const MatchFinder::MatchResu
 }
 
 void UseCheckedArithmeticCheck::fixDeclOperation(const MatchFinder::MatchResult &Result) {
-	const auto *MatchedExpr = Result.Nodes.getNodeAs<VarDecl>("DeclOperation");
+	const auto *MatchedDecl = Result.Nodes.getNodeAs<VarDecl>("DeclOperation");
 
-	const auto *dest = Result.Nodes.getNodeAs<DeclRefExpr>("dest");
 	const auto *opOne = Result.Nodes.getNodeAs<DeclRefExpr>("opOne");
 	const auto *opTwo = Result.Nodes.getNodeAs<DeclRefExpr>("opTwo");
 
-	auto replacement = "if(!ckd_add(&" + 
-		dest->getNameInfo().getAsString() + ", " +
+	const auto destType = MatchedDecl->getInit()->getType().getAsString();
+	const auto destName = MatchedDecl->getNameAsString();
+
+	auto replacement = destType + " " + destName + ";\n" +
+		"if(!ckd_add(&" + 
+		destName + ", " +
 		opOne->getNameInfo().getAsString() + ", " +
 		opTwo->getNameInfo().getAsString() + ")) {\n" +
-		"assert(false);\n" + "}\n";
+		"assert(0);\n" + "}\n";
 
-	DiagnosticBuilder Diag = diag(dest->getBeginLoc(), "use checked arithmetic");
-	Diag << FixItHint::CreateInsertion(dest->getLocation(), replacement);
-	Diag << IncludeInserter.createIncludeInsertion(Result.Context->getSourceManager().getFileID(MatchedExpr->getBeginLoc()), "<stdckdint.h>");
+	DiagnosticBuilder Diag = diag(MatchedDecl->getBeginLoc(), "use checked arithmetic");
+	Diag << FixItHint::CreateReplacement(MatchedDecl->getSourceRange(), replacement);
+	Diag << IncludeInserter.createIncludeInsertion(Result.Context->getSourceManager().getFileID(MatchedDecl->getBeginLoc()), "<stdckdint.h>");
 	return;
 }
 
