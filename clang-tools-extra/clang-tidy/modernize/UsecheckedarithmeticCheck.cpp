3a8c8d711fedf8b4ddd87799be2432edbf1485b9
John Samuels <johnsamuels1218@gmail.com>
2025-03-19 20:54:32 -0400

    Naive rewrite expressions with up to three arguments

--- clang-tools-extra/clang-tidy/modernize/UsecheckedarithmeticCheck.cpp
+++ clang-tools-extra/clang-tidy/modernize/UsecheckedarithmeticCheck.cpp
@@ -18,33 +18,35 @@ StatementMatcher makeNonDeclMatcher() {
              hasOperatorName("="), hasLHS(declRefExpr().bind("dest")),
              hasRHS(binaryOperator(
                         hasAnyOperatorName("+", "-", "*"),
-                        hasLHS(ignoringImpCasts(declRefExpr().bind("opOne"))),
-                        hasRHS(ignoringImpCasts(declRefExpr().bind("opTwo"))))
+                        hasLHS(ignoringImpCasts(declRefExpr().bind("argOne"))),
+                        hasRHS(ignoringImpCasts(declRefExpr().bind("argTwo"))))
                         .bind("operator")))
       .bind("NonDeclOperation");
 }
 
 DeclarationMatcher makeDeclMatcher() {
-  return varDecl(has(
-                     binaryOperator(
+  return varDecl(has(binaryOperator(
                          hasAnyOperatorName("+", "-", "*"),
-                         hasLHS(ignoringImpCasts(declRefExpr().bind("opOne"))),
-                         hasRHS(ignoringImpCasts(declRefExpr().bind("opTwo"))))
+                         hasLHS(ignoringImpCasts(declRefExpr().bind("argOne"))),
+                         hasRHS(ignoringImpCasts(declRefExpr().bind("argTwo"))))
                          .bind("operator")))
       .bind("DeclOperation");
 }
-/*
-DeclarationMatcher makeDeclMatcher() {
-  return binaryOperator(
-             hasOperatorName("="), hasLHS(declRefExpr().bind("dest")),
-             hasRHS(binaryOperator(
-                        hasAnyOperatorName("+", "-", "*"),
-                        hasLHS(ignoringImpCasts(declRefExpr().bind("opOne"))),
-                        hasRHS(ignoringImpCasts(declRefExpr().bind("opTwo"))))
-                        .bind("operator")))
-      .bind("NonDeclOperation");
+
+DeclarationMatcher makeMultiDeclMatcher() {
+  return varDecl(
+             has(binaryOperator(
+                     hasAnyOperatorName("+", "-", "*"),
+                     hasLHS(binaryOperator(hasAnyOperatorName("+", "-", "*"),
+                                           hasLHS(ignoringImpCasts(
+                                               declRefExpr().bind("argOne"))),
+                                           hasRHS(ignoringImpCasts(
+                                               declRefExpr().bind("argTwo"))))
+                                .bind("opOne")),
+                     hasRHS(ignoringImpCasts(declRefExpr().bind("argThree"))))
+                     .bind("opTwo")))
+      .bind("MultiDeclOperation");
 }
-*/
 
 std::string getCkdFunction(llvm::StringRef opStr) {
   if (opStr == "+") {
@@ -63,6 +65,7 @@ void UseCheckedArithmeticCheck::registerMatchers(MatchFinder *Finder) {
 
   Finder->addMatcher(traverse(TK_AsIs, makeNonDeclMatcher()), this);
   Finder->addMatcher(traverse(TK_AsIs, makeDeclMatcher()), this);
+  Finder->addMatcher(traverse(TK_AsIs, makeMultiDeclMatcher()), this);
 }
 
 void UseCheckedArithmeticCheck::fixNonDeclOperation(
@@ -70,8 +73,8 @@ void UseCheckedArithmeticCheck::fixNonDeclOperation(
   const auto *MatchedExpr = Result.Nodes.getNodeAs<Expr>("NonDeclOperation");
 
   const auto *dest = Result.Nodes.getNodeAs<DeclRefExpr>("dest");
-  const auto *opOne = Result.Nodes.getNodeAs<DeclRefExpr>("opOne");
-  const auto *opTwo = Result.Nodes.getNodeAs<DeclRefExpr>("opTwo");
+  const auto *argOne = Result.Nodes.getNodeAs<DeclRefExpr>("argOne");
+  const auto *argTwo = Result.Nodes.getNodeAs<DeclRefExpr>("argTwo");
 
   const auto *op = Result.Nodes.getNodeAs<BinaryOperator>("operator");
   const std::string ckdFunc = getCkdFunction(op->getOpcodeStr());
@@ -83,8 +86,8 @@ void UseCheckedArithmeticCheck::fixNonDeclOperation(
 
   auto replacement =
       "if(!" + ckdFunc + "(&" + dest->getNameInfo().getAsString() + ", " +
-      opOne->getNameInfo().getAsString() + ", " +
-      opTwo->getNameInfo().getAsString() + ")) {\n" + "assert(0);\n" + "}\n";
+      argOne->getNameInfo().getAsString() + ", " +
+      argTwo->getNameInfo().getAsString() + ")) {\n" + "assert(0);\n" + "}\n";
 
   DiagnosticBuilder Diag = diag(dest->getBeginLoc(), "use checked arithmetic");
   Diag << FixItHint::CreateInsertion(dest->getLocation(), replacement);
@@ -98,8 +101,8 @@ void UseCheckedArithmeticCheck::fixDeclOperation(
     const MatchFinder::MatchResult &Result) {
   const auto *MatchedDecl = Result.Nodes.getNodeAs<VarDecl>("DeclOperation");
 
-  const auto *opOne = Result.Nodes.getNodeAs<DeclRefExpr>("opOne");
-  const auto *opTwo = Result.Nodes.getNodeAs<DeclRefExpr>("opTwo");
+  const auto *argOne = Result.Nodes.getNodeAs<DeclRefExpr>("argOne");
+  const auto *argTwo = Result.Nodes.getNodeAs<DeclRefExpr>("argTwo");
 
   const auto *op = Result.Nodes.getNodeAs<BinaryOperator>("operator");
   const std::string ckdFunc = getCkdFunction(op->getOpcodeStr());
@@ -114,10 +117,54 @@ void UseCheckedArithmeticCheck::fixDeclOperation(
 
   auto replacement =
       destType + " " + destName + ";\n" + "if(!" + ckdFunc + "(&" + destName +
-      ", " + opOne->getNameInfo().getAsString() + ", " +
-      opTwo->getNameInfo().getAsString() + ")) {\n" + "assert(0);\n" + "}\n" +
-      destName + " = " + opOne->getNameInfo().getAsString() + " " +
-      op->getOpcodeStr().str() + " " + opTwo->getNameInfo().getAsString();
+      ", " + argOne->getNameInfo().getAsString() + ", " +
+      argTwo->getNameInfo().getAsString() + ")) {\n" + "assert(0);\n" + "}\n" +
+      destName + " = " + argOne->getNameInfo().getAsString() + " " +
+      op->getOpcodeStr().str() + " " + argTwo->getNameInfo().getAsString();
+
+  DiagnosticBuilder Diag =
+      diag(MatchedDecl->getBeginLoc(), "use checked arithmetic");
+  Diag << FixItHint::CreateReplacement(MatchedDecl->getSourceRange(),
+                                       replacement);
+  Diag << IncludeInserter.createIncludeInsertion(
+      Result.Context->getSourceManager().getFileID(MatchedDecl->getBeginLoc()),
+      "<stdckdint.h>");
+  return;
+}
+
+void UseCheckedArithmeticCheck::fixMultiDeclOperation(
+    const MatchFinder::MatchResult &Result) {
+
+  const auto *MatchedDecl = Result.Nodes.getNodeAs<VarDecl>("MultiDeclOperation");
+
+  const auto *argOne = Result.Nodes.getNodeAs<DeclRefExpr>("argOne");
+  const auto *argTwo = Result.Nodes.getNodeAs<DeclRefExpr>("argTwo");
+  const auto *argThree = Result.Nodes.getNodeAs<DeclRefExpr>("argThree");
+  const auto *opOne = Result.Nodes.getNodeAs<BinaryOperator>("opOne");
+  const auto *opTwo = Result.Nodes.getNodeAs<BinaryOperator>("opTwo");
+
+  const auto destType = MatchedDecl->getInit()->getType().getAsString();
+  const auto destName = MatchedDecl->getNameAsString();
+  const auto argOneName = argOne->getNameInfo().getAsString();
+  const auto argTwoName = argTwo->getNameInfo().getAsString();
+  const auto argThreeName = argThree->getNameInfo().getAsString();
+
+  const auto opOneFunc = getCkdFunction(opOne->getOpcodeStr());
+  const auto opTwoFunc = getCkdFunction(opTwo->getOpcodeStr());
+
+  if (opOneFunc == "" || opTwoFunc == "") {
+    llvm::outs() << "Error converting operation.\n";
+    return;
+  }
+
+  auto replacement = destType + " " + destName + ";\n";
+  replacement += "if(!" + opOneFunc + "(&" + destName + ", " + argOneName + ", " + argTwoName + ")) {\n";
+  replacement += "assert(0);\n}";
+  replacement += destName + " = " + argOneName + " " + opOne->getOpcodeStr().str() + " " + argTwoName + ";\n";
+
+  replacement += "if(!" + opTwoFunc + "(&" + destName + ", " + destName + ", " + argThreeName + ")) {\n";
+  replacement += "assert(0);\n}";
+  replacement += destName + " = " + destName + " " + opTwo->getOpcodeStr().str() + " " + argThreeName;
 
   DiagnosticBuilder Diag =
       diag(MatchedDecl->getBeginLoc(), "use checked arithmetic");
@@ -132,12 +179,12 @@ void UseCheckedArithmeticCheck::fixDeclOperation(
 void UseCheckedArithmeticCheck::check(const MatchFinder::MatchResult &Result) {
   // TODO this will be used to do the rewrite
 
-  if (const auto *MatchedExpr =
-          Result.Nodes.getNodeAs<Expr>("NonDeclOperation")) {
+  if (Result.Nodes.getNodeAs<Expr>("NonDeclOperation")) {
     fixNonDeclOperation(Result);
-  } else if (const auto *MatchedExpr =
-                 Result.Nodes.getNodeAs<VarDecl>("DeclOperation")) {
+  } else if (Result.Nodes.getNodeAs<VarDecl>("DeclOperation")) {
     fixDeclOperation(Result);
+  } else if (Result.Nodes.getNodeAs<VarDecl>("MultiDeclOperation")) {
+    fixMultiDeclOperation(Result);
   }
 
   return;
--- clang-tools-extra/clang-tidy/modernize/UsecheckedarithmeticCheck.h
+++ clang-tools-extra/clang-tidy/modernize/UsecheckedarithmeticCheck.h
@@ -30,6 +30,7 @@ public:
 private:
   utils::IncludeInserter IncludeInserter;
   void fixNonDeclOperation(const ast_matchers::MatchFinder::MatchResult &Result);
+  void fixMultiDeclOperation(const ast_matchers::MatchFinder::MatchResult &Result);
   void fixDeclOperation(const ast_matchers::MatchFinder::MatchResult &Result);
 };
 
